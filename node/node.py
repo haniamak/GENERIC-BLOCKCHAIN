769689e4ef53b95afc1e43f4814d59fef95a268b
RafalLeja <rafal@leja.org.pl>
2024-11-18 18:14:21 +0100

    statyczna lista węzłów

--- node/node.py
+++ node/node.py
@@ -7,8 +7,16 @@ import nodeList
 nL = nodeList.NodeList()
 
 # add nodes to nodeList
-nL.addNode('100.200.300.400')
-nL.addNode('200.300.400.500')
+nL.addNode('100.200.300.400', '1234')
+nL.addNode('200.300.400.500', '2345')
+
+# save nodeList to file
+fileName = 'nodes/nodes.json'
+nL.toFile(fileName)
+
+# load nodeList from file
+rNL = nodeList.NodeList()
+rNL.fromFile(fileName)
 
 # print nodeList
-print(nL)
+print(rNL)
--- node/nodeList.py
+++ node/nodeList.py
@@ -1,17 +1,45 @@
+import json
+
 class Node:
-    def __init__(self, ip: str):
+    def __init__(self, ip: str, port: str):
         self.ip = ip
+        self.port = port
 
     def __str__(self):
-        return self.ip
+        return self.ip + ':' + self.port
 
 
 class NodeList:
     def __init__(self):
         self.nodes = []
 
-    def addNode(self, ip):
-        self.nodes.append(Node(ip))
+    def addNode(self, ip, port):
+            self.nodes.append(Node(ip, port))
+
+    def removeNode(self, ip, port):
+        self.nodes = [node for node in self.nodes if node.ip != ip and node.port != port]
 
     def __str__(self):
         return '\n'.join([str(node) for node in self.nodes])
+
+    def toJson(self):
+        return json.dumps([{'ip': node.ip, 'port': node.port} for node in self.nodes])
+
+    def fromJson(self, jsonStr):
+        self.nodes = [Node(node['ip'], node['port']) for node in json.loads(jsonStr)]
+
+    def __iter__(self):
+        return iter(self.nodes)
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def toFile(self, filename):
+        with open(filename, 'w') as file:
+            file.write(self.toJson())
+
+    def fromFile(self, filename):
+        with open(filename, 'r') as file:
+            self.fromJson(file.read())
+
+
