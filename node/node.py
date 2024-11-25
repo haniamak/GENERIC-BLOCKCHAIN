f60d88c4ecce8a853b1ccde01a6fa02ca1a57924
rafal <rafal@debianPC.dom>
2024-11-25 17:05:48 +0100

    added sctp socket

--- node/node.py
+++ node/node.py
@@ -3,6 +3,8 @@ import nodeList
 import userList
 import time
 import keyboard
+import socket
+import sctp
 
 # np. blockList.initBlockList() # initialize blockList
 
@@ -10,8 +12,8 @@ import keyboard
 nL = nodeList.NodeList()
 
 # add nodes to nodeList
-nL.addNode('100.200.300.400', '1234', True)
-nL.addNode('200.300.400.500', '2345', False)
+nL.addNode('127.0.0.1', '10002', True)
+nL.addNode('127.0.0.1', '10003', False)
 
 # save nodeList to file
 fileName = 'nodes/nodes.json'
@@ -52,12 +54,50 @@ block2.save()
 
 print(block1.read())
 
+server_ip = ''
+server_port = ''
+
+print("input server IP (default = 127.0.0.1):")
+server_ip = input()
+if server_ip == '':
+    server_ip = '127.0.0.1'
+
+print("input server port (default = 10001):")
+server_port = input()
+if server_port == '':
+    server_port = '10001'
+
+sk = sctp.sctpsocket_tcp(socket.AF_INET)
+sk.bind((server_ip, int(server_port)))
+
+print("Server started at " + server_ip + ":" + server_port)
+
+for node in rNL.nodes:
+    print(f"Connecting to {node.ip}:{node.port}")
+    try:
+        sk.connect((node.ip, int(node.port)))
+        print(f"Connected to {node.ip}:{node.port}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+print("configuration finished")
+print("Starting loop, press ESC to exit")
+
+sampling_time = 2
+
 try:
     last_time = time.time()
     while True:
         current_time = time.time()
-        if current_time - last_time >= 10:
-            print("Working...")
+        if current_time - last_time >= sampling_time:
+            # check if we received any data
+            try:
+                data, addr = sk.recvfrom(1024)
+                print(f"Received data: {data}")
+            except:
+                print("No data received")
+
             last_time = current_time
         if keyboard.is_pressed('esc'):
             print("Esc pressed. Exiting loop.")
