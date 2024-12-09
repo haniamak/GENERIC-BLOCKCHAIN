cd3aa7866cdca8a168d144039e9ed72c9d0c9df9
bialykaloryfer <118823284+bialykaloryfer@users.noreply.github.com>
2024-12-09 20:49:38 +0100

    blockList.py - blockList include list of entries as data

--- node/blockList.py
+++ node/blockList.py
@@ -1,20 +1,21 @@
 from typing import List, Optional
+from entryList import Entry, EntryList
 import json
 
 
 class Block:
-    def __init__(self, data, prev_block: Optional['Block'] = None, next_block: Optional['Block'] = None):
-        self.data = data
+    def __init__(self, list_of_entries: Optional['EntryList'] = None, prev_block: Optional['Block'] = None, next_block: Optional['Block'] = None):
+        self.list_of_entries = list_of_entries
         self.prev_block = prev_block
         self.next_block = next_block
 
     def to_dict(self):
         return {
-            'data': self.data,
+            'entries': self.list_of_entries.to_dict()
         }
 
     def __str__(self):
-        return f"Block data: {self.data}"
+        return f"Block data: {self.list_of_entries.to_dict()}"
 
 
 class BlockList:
@@ -49,7 +50,11 @@ class BlockList:
             self.block_list = []
             prev = None
             for block_data in data:
-                block = Block(data=block_data["data"], prev_block=prev)
+                list_of_entries = EntryList()
+                for entry in block_data["entries"]:
+                    list_of_entries.add_entry(Entry(entry["entry_id"], entry["author_id"], entry["data"], entry["previous_entries"], entry["encryption_key"]))
+                block = Block(list_of_entries = list_of_entries, prev_block=prev)
+
                 if len(self.block_list) != 0:
                     self.block_list[-1].next_block = block
                 self.block_list.append(block)
@@ -63,3 +68,5 @@ class BlockList:
         text = "List of blocks: \n"
         for block in self.block_list: text += str(block) + "\n"
         return text
+
+
